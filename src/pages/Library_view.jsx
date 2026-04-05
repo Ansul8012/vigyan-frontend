7a11f5c4b05587793af6ad5341ebb8bc3354c165
Ansul <ansulsingh171@gmail.com>
2026-04-05 18:37:56 +0530

    done

--- src/pages/Library_view.jsx
+++ src/pages/Library_view.jsx
@@ -1,4 +1,4 @@
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useAuthStore } from "../store/useAuthStore";
 import { useState } from "react";
 
